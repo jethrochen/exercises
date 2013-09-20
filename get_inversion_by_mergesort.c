eb23adc38410315c411fbc6baa984e8a79a99e0c
cj <cj@cjpc.(none)>
2013-09-21 00:20:04 +0800

    final

--- get_inversion_by_mergesort.c
+++ get_inversion_by_mergesort.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int merge(int A[], int p, int m, int q)
+int count = 0;	//用于逆序对的计数
+
+void merge(int A[], int p, int m, int q)
 {
     int L[100];
     int R[100];
     int l, r;
     int i = 0, j = 0;
-    int count = 0;
 
     l = m - p + 1;
     r = q - m;
@@ -22,45 +23,38 @@ int merge(int A[], int p, int m, int q)
             A[p+i+j] = L[i++];
         else 
         {
+			count += (l-i);		//计算R中每个元素的逆序数
             A[p+i+j] = R[j++];
-            count++;
         }
     }
     while (i < l)
-    {
         A[p+i+j] = L[i++];
-        count++;
-    }
     while (j < r)
         A[p+i+j] = R[j++];
-
-    return count;
 }
 
 
-int get_inversion_by_mergesort(int A[], int l, int r)
+void get_inversion_by_mergesort(int A[], int l, int r)
 {
-    int count = 0;
     int mid = 0;
 
-    if (l < r)
+	if (l < r)
     {
         mid = (l + r) / 2;
         get_inversion_by_mergesort(A, l, mid);
         get_inversion_by_mergesort(A, mid+1, r);
-        count += merge(A, l, mid, r);
+        merge(A, l, mid, r);
     }
-
-    return count;
 }
 
 int main(void)
 {
     int A[10] = {5, 2, 6, 7, 1, 4, 9, 8, 0, 3};
-    int count = 0;
 
-    count = get_inversion_by_mergesort(A, 0, 9);
+    get_inversion_by_mergesort(A, 0, 9);
     printf("%d\n", count);
+	for (int i = 0; i < 10; ++i)
+		printf("%d ", A[i]);
 
     return 0;
 }
